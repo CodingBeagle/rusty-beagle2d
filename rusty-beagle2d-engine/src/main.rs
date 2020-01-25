cd13f183ea18b51d4bcd117631f726777078c7d2
CodingBeagle <kasper.rie@gmail.com>
2020-01-26 00:20:26 +0100

    Think I reached a nice design for hiding pointers in the GLFW API!

--- rusty-beagle2d-engine/src/main.rs
+++ rusty-beagle2d-engine/src/main.rs
@@ -1,17 +1,17 @@
 use rusty_beagle2d_glfw;
 
 fn main() {
-    let mut init_result = rusty_beagle2d_glfw::glfw_init();
+    let init_result = rusty_beagle2d_glfw::glfw_init();
 
     if !init_result {
         panic!("Failed to initialize GLFW!");
     }
 
-    let windowTitle = String::from("Rusyt Beagle! :!");
+    let window_title = String::from("Rusyt Beagle! :D");
 
-    let mainWindow = rusty_beagle2d_glfw::glfw_create_window(800, 600, windowTitle, None, None).unwrap();
+    let main_window = rusty_beagle2d_glfw::glfw_create_window(800, 600, window_title, None, None).unwrap();
 
-    while !rusty_beagle2d_glfw::glfw_window_should_close(mainWindow) {
+    while !rusty_beagle2d_glfw::glfw_window_should_close(&main_window) {
         rusty_beagle2d_glfw::glfw_poll_events();
     }
 }
--- rusty-beagle2d-glfw/src/lib.rs
+++ rusty-beagle2d-glfw/src/lib.rs
@@ -7,6 +7,23 @@ use std::ptr;
 #[repr(C)] pub struct GLFWmonitor { _private: [u8; 0] }
 #[repr(C)] pub struct GLFWwindow { _private: [u8; 0] }
 
+// By default, everything in Rust is private.
+// Except in two cases:
+// - Associated items in a "pub" Trait are public by default.
+// - Enum variants in a "pub" enum are also public by default.
+// Visibility in Rust works on a per-module basis.
+// Defining Window as "pub" means that it can be accessed from an external module.
+// If an item is private (like window_ptr field in Window), it can only be accessed by the current module.
+// This can be used to create module hierarchies exposing public APIs while hiding internal implementation details.
+// TODO: Read up on Modules! There's important things to be understood in regards to visibility in Rust
+pub struct Window {
+    window_ptr: *mut GLFWwindow
+}
+
+pub struct Monitor {
+    monitor_ptr: *mut GLFWmonitor
+}
+
 #[link(name = "glfw3-x64-debug", kind="dylib")]
 // The "extern" block is used to facilitate creation and use of an FFI (Foreign Function Interface)
 // An FFI is a way for a programming language to define functions and enable a different (foreign)
@@ -37,17 +54,19 @@ pub fn glfw_init() -> bool {
     }
 }
 
-pub fn glfw_window_should_close(window: &mut GLFWwindow) -> bool {
+pub fn glfw_window_should_close(window: &Window) -> bool {
     unsafe {
-        glfwWindowShouldClose(window as *mut GLFWwindow) == 1
+        glfwWindowShouldClose(window.window_ptr) == 1
     }
 }
 
 // TODO: Jesus read this https://stackoverflow.com/questions/24145823/how-do-i-convert-a-c-string-into-a-rust-string-and-back-via-ffi
-// TODO: Is there any way not to use mutable references in the safe wrapper? I would have to convert immutable reference to mutable C pointer type somehow.
+// TODO: Is there any way NOT to use mutable references in the safe wrapper? I would have to convert immutable reference to mutable C pointer type somehow.
 // TODO: Seriously need to read up on lifetimes here: https://doc.rust-lang.org/book/ch10-03-lifetime-syntax.html
 // TODO: Read up on Options: https://doc.rust-lang.org/std/option/
-pub fn glfw_create_window(width: i32, height: i32, title: String, monitor: Option<&mut GLFWmonitor>, share: Option<&mut GLFWwindow>) -> Option<&'static mut GLFWwindow> {
+// TODO: Remember to read up on CString some more here: https://doc.rust-lang.org/std/ffi/struct.CString.html
+// TODO: Maybe return some type of result object here? With either a window, or an error in case something went wrong!
+pub fn glfw_create_window(width: i32, height: i32, title: String, monitor: Option<&Monitor>, share: Option<&Window>) -> Option<Window> {
     let title_c_string =  CString::new(title).expect("Failed to create title string!");
 
     let result = unsafe {
@@ -56,11 +75,11 @@ pub fn glfw_create_window(width: i32, height: i32, title: String, monitor: Optio
             height, 
             title_c_string.as_ptr(), 
             match monitor {
-                Some(x) => x as *mut GLFWmonitor,
+                Some(x) => x.monitor_ptr as *mut GLFWmonitor,
                 None => ptr::null_mut() as *mut GLFWmonitor
             }, 
             match share {
-                Some(x) => x as *mut GLFWwindow,
+                Some(x) => x.window_ptr as *mut GLFWwindow,
                 None => ptr::null_mut() as *mut GLFWwindow
             })
         };
@@ -68,8 +87,10 @@ pub fn glfw_create_window(width: i32, height: i32, title: String, monitor: Optio
     if result.is_null() {
         return None;
     }
+    
+    let created_window = Window {
+        window_ptr: result
+    };
 
-    unsafe {
-        result.as_mut()
-    }
+    Some(created_window)
 }
